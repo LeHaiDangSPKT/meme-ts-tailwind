49155f0a3d4e162536a18268ed6fbc3accb6fd4f
LeHaiDangSPKT <0509dangle@gmail.com>
2022-12-24 15:27:02 +0700

    Config typescript and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-black">
+      <h1 className='text-cyan-600'>Hello anh em</h1>
+    </div>
+  );
+}
+
+export default App;
